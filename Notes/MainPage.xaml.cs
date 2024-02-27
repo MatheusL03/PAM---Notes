8631ecf284ca23a7daf877b9472f8faa9ec50ae3
MatheusL03 <matheusl_3@hotmail.com>
2024-02-26 21:36:38 -0300

    chore add gitignore

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        string nome = " ";
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnNameClicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            GreetingLabel.Text = $"Hello, {nome}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+    }
+
+}
